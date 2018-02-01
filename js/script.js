34878b202ad8791485fce48ca3241497491c82e2
Anna Perkowska <perkowska.a@gmail.com>
2018-02-01 23:12:05 +0100

    Change script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var femaleNames = ["Asia", "Kasia", "Ola", "Jola"],
+	maleNames = ["Piotrek", "Marek", "Arek", "Jarek"],
+	allNames = femaleNames.concat(maleNames),
+	nextName = prompt("Enter your name");
+
+console.log(allNames);
+function checkTableContent
