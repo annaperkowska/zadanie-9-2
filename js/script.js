4918dc6fcef723a468a1abecfb700fbda806d423
Anna Perkowska <perkowska.a@gmail.com>
2018-02-02 09:29:10 +0100

    Change script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var femaleNames = ["Asia", "Kasia", "Ola", "Jola"],
 	maleNames = ["Piotrek", "Marek", "Arek", "Jarek"],
 	allNames = femaleNames.concat(maleNames),
-	nextName = prompt("Enter your name");
+	newName = prompt("Enter your name");
 
 console.log(allNames);
-function checkTableContent
+if (allNames.indexOf(newName) === -1) {
+	allNames.push(newName);
+}
